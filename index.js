f8ef54a97dcd497518762d1a2ee8c7f109a7a7b5
manueldezman <adelekeabdulganiy@gmail.com>
2024-06-22 00:13:08 +0100

    feat: replaced user selection prompt with buttons

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ function getComputerChoice(randomNumber) {
     return computerSelection;
 } 
 
-function playRound(playerSelection, computerSelection, PlayerScore, ComputerScore) {
+function playRound(playerSelection, computerSelection, PlayerScore, ComputerScore, round) {
     playerSelection = playerSelection.toLowerCase();
+    console.log(computerSelection);
     computerSelection = computerSelection.toLowerCase();
+    if (PlayerScore == 5){
+        return`
+                You Win!
+
+                Final Score:
+                
+                Player: ${PlayerScore}
+                Computer: ${ComputerScore}
+        `
+    }
+    else if (ComputerScore == 5) {
+        return `
+            You lose!
+
+            Final Score:
+            
+            Player: ${PlayerScore}
+            Computer: ${ComputerScore}
+`
+    }
 
     if (playerSelection === computerSelection) {
             ComputerScore=Computer;
             PlayerScore=Player;
+            round_++;
+            round = round_;
         
         return `It's a tie!
                 Playerscore: ${PlayerScore}
@@ -20,6 +43,8 @@ function playRound(playerSelection, computerSelection, PlayerScore, ComputerScor
             Computer++;
             ComputerScore=Computer;
             PlayerScore=Player;
+            round_++;
+            round = round_;
             
 
             return `You lose! Paper beats Rock
@@ -29,6 +54,8 @@ function playRound(playerSelection, computerSelection, PlayerScore, ComputerScor
             Player++;
             PlayerScore=Player;
             ComputerScore=Computer;
+            round_++;
+            round = round_;
             
             return `You win! Rock beats Scissors
                     Playerscore: ${PlayerScore}
@@ -40,6 +67,8 @@ function playRound(playerSelection, computerSelection, PlayerScore, ComputerScor
             Player++;
             PlayerScore = Player;
             ComputerScore=Computer;
+            round_++;
+            round = round_;
             
             return `You win! paper beats Rock
                     Playerscore: ${PlayerScore}
@@ -49,6 +78,8 @@ function playRound(playerSelection, computerSelection, PlayerScore, ComputerScor
             Computer++;
             ComputerScore = Computer;
             PlayerScore = Player;
+            round_++;
+            round = round_;
 
             return `You lose! Scissors beats Paper
                     Playerscore: ${PlayerScore}
@@ -60,6 +91,8 @@ function playRound(playerSelection, computerSelection, PlayerScore, ComputerScor
             Player++;
             PlayerScore = Player;
             ComputerScore=Computer;
+            round_++;
+            round = round_;
             
 
             return `You win! Scissors beats Paper
@@ -70,85 +103,69 @@ function playRound(playerSelection, computerSelection, PlayerScore, ComputerScor
             Computer++;
             ComputerScore = Computer;
             PlayerScore = Player;
+            round_++;
+            round = round_;
             
             return `You lose! Rock beats Scissors
                     Playerscore: ${PlayerScore}
                     Computerscore: ${ComputerScore}`;
         }
     }
+
+
 }
-
-function PlayGame() {
     
-    let round;
-    let roundLimit = 5;
     
 
-
-    for (round = 1; round <= roundLimit; round++) {
-        console.log("Round: ", round);
-        
-        
-        let randomNumber = Math.floor(Math.random() * choice.length);
-
-        let playerSelection = prompt("Input your pick: rock, paper or scissors? ");
-        
-        console.log("Player selection: ", playerSelection);
-
-        console.log("Computer selection: ", getComputerChoice(randomNumber));
-
-        console.log("Result: ", playRound(playerSelection, computerSelection, PlayerScore, ComputerScore));
-
-    
-    }
-            PlayerScore = Player;
-            ComputerScore=Computer;
-    
-
-    if (PlayerScore > ComputerScore){
-        console.log(`
-                    You Win!
-
-                    Final Score:
-                    
-                    Player: ${PlayerScore}
-                    Computer: ${ComputerScore}
-        `)
-    }
-    else if (ComputerScore > PlayerScore) {
-                console.log(`
-                    You lose
-
-                    Final Score:
-                    
-                    Player: ${PlayerScore}
-                    Computer: ${ComputerScore}
-        `)
-    }
-    else {
-        console.log(`
-                    It ends in  a draw
-
-                    Final Score:
-                    
-                    Player: ${PlayerScore}
-                    Computer: ${ComputerScore}
-        `)
-    }
-    
-
-    
-}
-
-let computerSelection
+let computerSelection;
 let PlayerScore = 0;
 let ComputerScore = 0;
 
 let Player = PlayerScore;
 let Computer = ComputerScore;
+let round = 1;
+let round_ = round;
 
+let result = document.querySelector('.result');
 
 let choice = ["Rock", "Paper", "Scissors"];
 
-PlayGame();
-  
+const div = document.querySelector('#container');
+
+    div.addEventListener('click', function(event) {
+        let playerSelection;
+        console.log(event.target.id);
+    switch(event.target) {
+        case rock:
+            playerSelection = "rock";
+            break;
+        case paper:
+            playerSelection = "paper";
+            break;
+        case scissors:
+            playerSelection = "scissors";
+            break;
+        default:
+            playerSelection = "none";
+            break;
+    } 
+    
+   
+
+    console.log("Player selection: ", playerSelection);
+
+
+
+    let randomNumber = Math.floor(Math.random() * choice.length);
+
+    
+    console.log("Player selection: ", playerSelection);
+
+    console.log("Computer selection: ", getComputerChoice(randomNumber));
+        PlayerScore = Player;
+        ComputerScore=Computer;
+    
+
+    result.textContent = `${playRound(playerSelection, computerSelection, PlayerScore, ComputerScore, round)}`;
+}
+    );
